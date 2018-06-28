b26a9e9d90ba19ed81d60489f4280f26995bc5f0
Tafadzwa Makuyana <tafadzwa.makuyana@students.stamford.edu>
2018-06-28 21:21:58 +0700

    static ui with dummy content added, Navigation, Event components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import Navbar from "../../features/nav/NavBar/NavBar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-events</h1>
+      <div>
+        <Navbar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
 }
 
 export default App;
-
